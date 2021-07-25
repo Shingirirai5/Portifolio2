72b385534a931dc55a2e21e574f9c1497065ed43
Shingirirai5 <71317833+Shingirirai5@users.noreply.github.com>
2021-07-25 23:39:01 +0200

    mns

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Programmer", "Graphic Designer"],
+        strings: ["Web Development", "Graphic Designs", "Social Media Marketing"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
